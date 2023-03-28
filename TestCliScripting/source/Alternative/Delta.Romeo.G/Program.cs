9960135df9351a2c7b51fbc1a803b4d20f2db776
Nigel Major <NigelWGMajor@users.noreply.github.com>
2023-03-28 09:45:12 -0500

    Added reflection to Alternative project

--- TestCliScripting/source/Alternative/Delta.Romeo.G/Program.cs
+++ TestCliScripting/source/Alternative/Delta.Romeo.G/Program.cs
@@ -7,9 +7,15 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddGrpc();
+builder.Services.AddGrpcReflection();
 
 var app = builder.Build();
 
+IWebHostEnvironment env = app.Environment;
+if (env.IsDevelopment())
+{
+    app.MapGrpcReflectionService();
+}
 // Configure the HTTP request pipeline.
 app.MapGrpcService<GreeterService>();
 app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
